9f4bc582731f2bce3c93a01783f7dfbf916bdd4e
Apex862-2 <140745064+Apex862-2@users.noreply.github.com>
2023-07-27 13:51:32 -0400

    Create Colors.py
    this is the first faze of the code

---
+++ Colors.py
@@ -0,0 +1,77 @@
+"""
+Each Color Has Its Own Type
+{color} = color in numbers
+Black = 000, 000, 000
+
+
+{color}s = color in numbers in a string
+Blacks = "000, 000, 000"
+
+
+
+{color}t = color in a tuple
+Blackt = (000, 000, 000)
+
+
+
+{color}ts = color in a tuple as a string
+Black = ("000, 000, 000")
+"""
+
+
+def Im_Lost():
+    print(
+        "Each Color Has Its Own Type\n"
+        "{color} = color in numbers\n"
+        "Black = 000, 000, 000"
+        "\n"
+        "\n"
+        "{color}s = color in numbers in a string\n"
+        "Blacks = \"000, 000, 000\""
+        "\n"
+        "\n"
+        "\n"
+        "{color}t = color in a tuple\n"
+        "Blackt = (000, 000, 000)\n"
+        "\n"
+        "\n"
+        "\n"
+        "{color}ts = color in a tuple as a string\n"
+        "Black = (\"000, 000, 000\")"
+        "\n"   
+        "But {color} is a color such as Black"
+    )
+
+
+Black = 000, 000, 000
+Blacks = "000, 000, 000"
+Blackt = (000, 000, 000)
+Blackts = ("000, 000, 000")
+
+# Reds___________________________________________________________________________________________________
+
+Red = 255, 0, 0
+
+
+Imperial_red = 237, 41, 57
+
+
+Folly = 255, 0, 79
+
+
+Persian = 202, 52, 51
+
+
+Strawberry = 251, 41, 67
+
+
+Carmine = 255, 0, 64
+
+
+Alarzin_crimson = 227, 38, 54
+
+
+Neon_red = 255, 7, 58
+
+
+Cardinals_red = 189, 32, 49
