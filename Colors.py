26cf4465a7261c2be98fe94d07ecfa31483860c7
Apex862-2 <140745064+Apex862-2@users.noreply.github.com>
2023-08-03 00:39:06 -0400

    Update Colors.py

--- Colors.py
+++ Colors.py
@@ -19,41 +19,11 @@ Black = ("000, 000, 000")
 """
 
 
-def Im_Lost():
-    print(
-        "Each Color Has Its Own Type\n"
-        "{color} = color in numbers\n"
-        "Black = 000, 000, 000"
-        "\n"
-        "\n"
-        "{color}s = color in numbers in a string\n"
-        "Blacks = \"000, 000, 000\""
-        "\n"
-        "\n"
-        "\n"
-        "{color}t = color in a tuple\n"
-        "Blackt = (000, 000, 000)\n"
-        "\n"
-        "\n"
-        "\n"
-        "{color}ts = color in a tuple as a string\n"
-        "Black = (\"000, 000, 000\")"
-        "\n"   
-        "But {color} is a color such as Black"
-    )
-
-
 Black = 000, 000, 000
-Blacks = "000, 000, 000"
-Blackt = (000, 000, 000)
-Blackts = ("000, 000, 000")
 
-# Reds___________________________________________________________________________________________________
 
 Red = 255, 0, 0
-Reds = "255, 0, 0"
-Redt = (255, 0, 0)
-Redts = ("255, 0, 0") 
+
 
 Imperial_red = 237, 41, 57
 
